c3ca9f90bc74b7ba6f800d75025bc4053bb8401b
dandydanderson <davanthander@gmail.com>
2020-04-08 16:48:50 -0400

    Spring Security and JWT setup, need to integrate them

---
+++ src/main/java/com/revature/repositories/ApplicationUserRepository.java
@@ -0,0 +1,11 @@
+package com.revature.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.revature.beans.ApplicationUser;
+
+
+public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, String> {
+	
+	
+
+}
---
+++ src/main/java/com/revature/security/ApplicationSecurityConfig.java
@@ -0,0 +1,67 @@
+package com.revature.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.revature.jwt.JwtUsernameAndPasswordAuthenticationFilter;
+import com.revature.services.ApplicationUserService;
+
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private final PasswordEncoder passwordEncoder;
+	private final ApplicationUserService appService;
+	
+	
+	
+	
+	@Autowired
+	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, ApplicationUserService appService) {
+		this.passwordEncoder = passwordEncoder;
+		this.appService = appService;
+	}
+	
+
+	/**
+	 * This method uses our custom Application User Service to get the principle. For JPA you have to use a custom service, 
+	 * as spring security doesn't have an out of the box solution. 
+	 */
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.userDetailsService(appService);
+		
+	}
+	
+	/**
+	 * This is where our roles get the endpoints they are authorized to view set. 
+	 */
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			//.sessionManagement()
+			//.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			//.addFilter(new JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager))
+			.authorizeRequests()
+			.antMatchers("/**").permitAll();
+		//	.antMatchers("/**").hasAnyRole("USER", "ADMIN");
+			
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return passwordEncoder;
+	}
+	
+}
+  
